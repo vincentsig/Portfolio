aa725ae3ecde04c3e0a9135afa9b37bae0b999f9
unknown <vincent.signoret.mail@gmail.com>
2021-04-05 14:02:41 -0400

    add and configure phpunit and SQL memory database

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class HomeControllerTest extends KernelTestCase
+{
+    private $entityManager;
+
+    protected function setUp() :void
+    {
+        $kernel = self::bootKernel();
+
+        DatabasePrimer::prime($kernel);
+
+    $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    public function testItWorks()
+        {
+            $this->assertTrue(true);
+        }
+    
+}
