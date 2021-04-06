3d7f972cb3b59f860cdc578ecd1fc529af84cb7e
unknown <vincent.signoret.mail@gmail.com>
2021-04-06 17:55:46 -0400

    style: psr12 in HomeCOntroller and SkillController

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -8,14 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class SkillController extends AbstractController
 {
     
     /**
      * @Route("/skill", name="skill")
      */
-    public function index(SkillRepository $repo ): Response
+    public function index(SkillRepository $repo): Response
     {
         return $this->render('skill/index.html.twig', [
             'controller_name' => 'SkillController',
