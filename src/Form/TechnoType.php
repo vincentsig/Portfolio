d1e1e9c2fda0cc79da138d3b70c0ffa0313fb28f
unknown <vincent.signoret.mail@gmail.com>
2021-04-07 13:45:14 -0400

    feat: add CRUD for techno

--- src/Form/TechnoType.php
+++ src/Form/TechnoType.php
@@ -2,27 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Skill;
+use App\Entity\Techno;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class SkillType extends AbstractType
+class TechnoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('media', MediaType::class, [
-                'required' => false,
-            ]);
+            ->add('name')
+            ->add('slug')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Skill::class,
+            'data_class' => Techno::class,
         ]);
     }
 }
